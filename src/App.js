afa77867b6e67dc8d2b86a82d9dd522dd2973c96
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-17 01:48:44 -0400

    initial react setup done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Staff from './components/pages/Staff';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/staff" exact={true} component={Staff} />
+        </div>
+      </Router>
     );
   }
 }
