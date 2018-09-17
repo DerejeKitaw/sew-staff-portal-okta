2a0b148fec7dc6a7cea3814ea98efeb92a7bab21
Dereje Kitaw <dereje.kitaw@gmail.com>
2018-09-17 02:41:24 -0400

    okta login added

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Install dependency
+# [Install dependency](https://developer.okta.com/code/react/okta_react_sign-in_widget)
 ```
 npm i react-router-dom @okta/okta-react @okta/okta-signin-widget
 ```
--- src/App.js
+++ src/App.js
@@ -1,19 +1,46 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import Staff from './components/pages/Staff';
+import Login from './components/auth/Login';
+import {clientId, issuer, baseUrl} from './config';
+// const clientId = require('./config').clientId;
 import './App.css';
+
+
+function onAuthRequired({ history }) {
+  history.push('/login')
+}
+
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <Navbar />
-          <Route path="/" exact={true} component={Home} />
-          <Route path="/staff" exact={true} component={Staff} />
-        </div>
+        <Security
+          issuer={issuer}
+          client_id={clientId}
+          redirect_uri={window.location.origin + '/implicit/callback'}
+          onAuthRequired={onAuthRequired}
+        >
+          <div className="App">
+            <Navbar />
+            <Route path="/" exact={true} component={Home} />
+            <SecureRoute path="/staff" exact={true} component={Staff} />
+
+            <Route
+                path="/login"
+                render={() => (
+                  <Login baseUrl={baseUrl} />
+                )}
+              />
+            <Route path="/implicit/callback" component={ImplicitCallback} />
+            
+
+          </div>
+        </Security>
       </Router>
     );
   }
